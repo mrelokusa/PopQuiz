17f7cfa6be42e6e92ea6ae5dc6a9ab04dbedceec
elokusa-stb <elokusaz@silvertreebrands>
2026-02-22 02:00:54 +0200

    feat: add LoudCandyLogo to app footer
    Places the Loud Candy parent brand logo in the bottom-right corner of every screen.

--- components/Layout/AppFooter.tsx
+++ components/Layout/AppFooter.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import LoudCandyLogo from '../ui/LoudCandyLogo';
 
 const AppFooter: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 p-4 text-center pointer-events-none flex justify-center items-end px-6">
+    <footer className="fixed bottom-0 left-0 right-0 p-4 pointer-events-none flex justify-between items-end px-6">
       <span className="bg-white border-2 border-black px-3 py-1 text-[10px] font-black uppercase tracking-widest shadow-neo-sm transform rotate-2 inline-block">
         Made with PopQuiz
       </span>
+      <LoudCandyLogo size="small" />
     </footer>
   );
 };
